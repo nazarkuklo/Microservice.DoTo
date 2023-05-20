fee8bd71c2be302e4685359e60ebfd5a3892d7eb
Nazarii Kuklo <n.kuklo@avidi.tech>
2023-05-20 13:44:27 +0300

    feature: Add Mediator to app

---
+++ Microservice.TaskManagement.Application/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microservice.TaskManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microservice.TaskManagement.Persistence.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : BaseEntity<int>, new()
+    {
+        public Task<TEntity> GetByIdAsync(int Id);
+        Task<List<TEntity>> GetAllAsync();
+        Task<int> AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task RemoveAsync(int id);
+    }
+}
---
+++ Microservice.TaskManagement.Persistence/Repositories/Repository.cs
@@ -0,0 +1,57 @@
+﻿using Microservice.TaskManagement.Domain.Entities;
+using Microservice.TaskManagement.Persistence.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microservice.TaskManagement.Persistence.Repositories
+{
+    public class Repository<TEntity, TContext> : IRepository<TEntity>
+        where TEntity : BaseEntity<int>, new()
+        where TContext : DbContext
+    {
+        private readonly TContext _context;
+        public TContext Context => _context;
+
+        public Repository(TContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public Task<List<TEntity>> GetAllAsync()
+        {
+           return Task.FromResult(_context.Set<TEntity>().ToList());
+        }
+
+        public async Task<TEntity> GetByIdAsync(int Id)
+        {
+            return await _context.Set<TEntity>().FirstOrDefaultAsync(x => x.Id == Id);
+        }
+
+        public async Task<int> AddAsync(TEntity entity)
+        {
+            await _context.Set<TEntity>().AddAsync(entity);
+
+            return entity.Id;
+        }
+
+        public Task UpdateAsync(TEntity entity)
+        {
+            _context.Set<TEntity>().Update(entity);
+
+            return Task.CompletedTask;
+        }
+
+        public Task RemoveAsync(int id)
+        {
+           var entities = _context.Set<TEntity>();
+            entities.Remove(entities.FirstOrDefault(x => x.Id == id));
+            return Task.CompletedTask;
+        }
+
+
+    }
+}
